973025151bbe695e14b749ee38661610ec746307
FuneralStream <mohnach2000@gmail.com>
2024-09-24 07:10:59 +0300

    привет

--- src/main/java/com/example/demo/controller/PasswordController.java
+++ src/main/java/com/example/demo/controller/PasswordController.java
@@ -1,17 +1,33 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Password;
+import com.example.demo.repository.PasswordRepository;
 import com.example.demo.service.PasswordService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class PasswordController {
+    private final PasswordRepository passwordRepository;
+    private final PasswordService passwordService;
+
     @Autowired
-    private PasswordService passwordService;
+    public PasswordController(PasswordRepository passwordRepository, PasswordService passwordService) {
+        this.passwordRepository = passwordRepository;
+        this.passwordService = passwordService;
+    }
 
     @PostMapping("/generate-passwords")
     public void generatePasswords() {
         passwordService.generateAndSavePasswords();
+    }
+
+    @GetMapping("/get-passwords")
+    public List<Password> getAllPasswords() {
+        return passwordRepository.findAll();
     }
 }
--- src/main/java/com/example/demo/repository/PasswordRepository.java
+++ src/main/java/com/example/demo/repository/PasswordRepository.java
@@ -3,7 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.entity.Password;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface PasswordRepository extends JpaRepository<Password, Long> {
+    List<Password> findAll();
 }
